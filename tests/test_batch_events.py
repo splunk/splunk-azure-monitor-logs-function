218164b3c80f8781b1748b4ee38a14b1e1778ef0
sxie <sxie@splunk.com>
2021-08-11 13:50:56 -0700

    explicitly assert the output of the target function
    The original tests only assert the length of the output but the content can be anything. For example, the old tests still pass after changing the return line of `batch_events` to `return [1] * len(batches)`.

--- tests/test_batch_events.py
+++ tests/test_batch_events.py
@@ -12,40 +12,40 @@ class TestBatchEvents(unittest.TestCase):
 
     def test_batch_events_zero_logs_zero_batches(self):
         events = []
-        self.assertEqual(0, len(batch_events(events)))
+        self.assertEqual([], batch_events(events))
 
     def test_batch_events_single_log_fit_in_single_batch(self):
         events = create_events(count=1, size=1)
-        self.assertEqual(1, len(batch_events(events)))
+        self.assertEqual(['a'], batch_events(events))
 
     def test_batch_events_single_log_fit_exactly_in_single_batch(self):
-        events = create_events(count=1, size=2)
-        self.assertEqual(1, len(batch_events(events)))
+        events = create_events(count=1, size=3)
+        self.assertEqual(['aaa'], batch_events(events))
 
     def test_batch_events_single_too_large_log_fit_in_single_batch(self):
         events = create_events(count=1, size=4)
-        self.assertEqual(1, len(batch_events(events)))
+        self.assertEqual(['aaaa'], batch_events(events))
 
     def test_batch_events_multiple_logs_fit_in_single_batch(self):
         events = create_events(count=2, size=1)
-        self.assertEqual(1, len(batch_events(events)))
+        self.assertEqual(['aa'], batch_events(events))
 
     def test_batch_events_multiple_logs_fit_exactly_in_single_batch(self):
         events = create_events(count=3, size=1)
-        self.assertEqual(1, len(batch_events(events)))
+        self.assertEqual(['aaa'], batch_events(events))
 
     def test_batch_events_multiple_logs_overflows_to_two_batches(self):
         events = create_events(count=4, size=1)
-        self.assertEqual(2, len(batch_events(events)))
+        self.assertEqual(['aaa', 'a'], batch_events(events))
 
     def test_batch_events_multiple_logs_overflows_to_three_batches(self):
         events = create_events(count=9, size=1)
-        self.assertEqual(3, len(batch_events(events)))
+        self.assertEqual(['aaa', 'aaa', 'aaa'], batch_events(events))
 
     def test_batch_events_multiple_logs_first_large_others_not_overflows(self):
         events = create_events(count=1, size=4)
         events.extend(create_events(count=3, size=1))
-        self.assertEqual(2, len(batch_events(events)))
+        self.assertEqual(['aaaa', 'aaa'], batch_events(events))
 
     def test_create_event(self):
         event = create_event(3)
