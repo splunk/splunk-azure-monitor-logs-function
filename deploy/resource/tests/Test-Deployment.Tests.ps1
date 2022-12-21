78abaf33f753a462d163cbe2c4ae7baef995b7fd
Ivneet Kaur <ikaur@splunk.com>
2022-12-21 21:05:07 +0000

    ADDON-58534:[ARM] Resource logs template to support multiple regions

--- deploy/resource/tests/Test-Deployment.Tests.ps1
+++ deploy/resource/tests/Test-Deployment.Tests.ps1
@@ -3,10 +3,6 @@ Param(
     [Parameter(Mandatory = $false)]
     [string]
     $TemplateFile = '../splunk-resource-logs-deploy-resources.json',
-    # Location
-    [Parameter(Mandatory = $false)]
-    [string]
-    $Location = "East US",
     # FunctionPackageURL
     [Parameter(Mandatory = $false)]
     [string]
@@ -15,6 +11,14 @@ Param(
     [Parameter(Mandatory = $false)]
     [string]
     $ServicePrincipalObjectId = 'b03d5072-69eb-4165-b453-c1b1a33de468',
+    # Regions
+    [Parameter(Mandatory = $false)]
+    [array]
+    $Regions = @('eastus', 'westus'),
+    # Location
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Location = $Regions[0],
     # SCDM ID
     [Parameter(Mandatory = $true)]
     [string]
@@ -24,19 +28,19 @@ Param(
 
 Describe "Azure ARM Template Unit Tests" {
     BeforeAll {
-        $resourcePrefix = 'SplunkDMDataIngest-'
+        $rgNamePrefix = 'SplunkDMDataIngest-'
         $consumerGroupName = "splk-resource-logs-consumer-group"
         $eventHubAuthRuleListen = "splk-resource-logs-eventhub-auth-listen"
         $eventHubAuthRuleSend = "splk-resource-logs-eventhub-auth-send"
         $eventHubName =  "splk-resource-logs-eventhub"
-        $eventHubNamespaceName =  "splkResLogsEH" + $SCDMInputId
-        $functionName = "splkResLogsFn" + $SCDMInputId
+        $eventHubNamespaceNamePrefix =  "splkResLogsEH"
+        $functionNamePrefix = "splkResLogsFn"
 
         $deploymentResult = Get-AzDeploymentWhatIfResult `
             -Name "UnitTestDeployment" `
             -Location $Location `
             -TemplateFile $TemplateFile `
-            -TemplateParameterObject @{hecUrl = "http://localhost/"; hecToken = "i-am-a-token"; region = $Location; scdmInputId = $SCDMInputId; servicePrincipalObjectId = $ServicePrincipalObjectId; functionPackageURL = $FunctionPackageURL; resourceTags = @{tagKey = "tagValue"}}
+            -TemplateParameterObject @{hecUrl = "http://localhost/"; hecToken = "i-am-a-token"; regions = $Regions; scdmInputId = $SCDMInputId; servicePrincipalObjectId = $ServicePrincipalObjectId; functionPackageURL = $FunctionPackageURL; resourceTags = @{tagKey = "tagValue"}}
     }
 
     Context "Deployment Successful" {
@@ -69,14 +73,28 @@ Describe "Azure ARM Template Unit Tests" {
         It "Resource Group Created" {
             $resourceGroups = $resourceMap['Microsoft.Resources/resourceGroups']
 
-            $resourceGroups.Count | Should -Be 1
-            $resourceGroups[0].ChangeType | Should -Be "Create"
+            $resourceGroups.Count | Should -Be $regions.Count
 
-            $name = $resourcePrefix + $SCDMInputId
-            $resourceGroups[0].After["name"].Value | Should -Be $name
-            $resourceGroups[0].After["tags"]["SplunkDMInputId"].Value | Should -Be $SCDMInputId
-            $resourceGroups[0].After["tags"]["SplunkDMDeletionOrder"].Value | Should -Be "10"
-            $resourceGroups[0].After["tags"]["tagKey"].Value | Should -Be "tagValue"
+            $rgNamesListExpected = @()
+
+            foreach ($region in $regions) {
+                $rgNameExpected = $rgNamePrefix + $SCDMInputId +  '-' + $region
+                $rgNamesListExpected += $rgNameExpected
+            }
+
+            $rgNamesListActual = @()
+
+            foreach($resourceGroup in $resourceGroups) {
+                $resourceGroup.ChangeType | Should -Be "Create"
+                $resourceGroup.After["tags"]["SplunkDMInputId"].Value | Should -Be $SCDMInputId
+                $resourceGroup.After["tags"]["tagKey"].Value | Should -Be "tagValue"
+                $rgNamesListActual += $resourceGroup.After["name"].Value
+            }
+
+            # actual order of creation can't be guaranteed since resources are deployed in parallel
+            # hence, comparing actual names observed with expected names without order
+            $CompareRgNames = Compare-Object $rgNamesListActual $rgNamesListExpected -PassThru
+            "$CompareRgNames" | Should -Be ""
         }
 
         It "Role Definitions Created" {
@@ -95,7 +113,11 @@ Describe "Azure ARM Template Unit Tests" {
             $properties["type"].Value | Should -Be "CustomRole"
 
             $properties["assignableScopes"].Count | Should -Be 1
-            $properties["assignableScopes"][0].Value | Should -Be $resourceMap['Microsoft.Resources/resourceGroups'][0].After["id"].Value
+            $resourceg = $resourceMap['Microsoft.Resources/resourceGroups'][0].After["id"].Value
+            $resinfo = $resourceg.Split("/")
+            $subscription ='/' + $resinfo[1] + '/' + $resinfo[2]
+
+            $properties["assignableScopes"][0].Value | Should -Be $subscription
 
             $permissions = $properties["permissions"]
             $permissions.Count | Should -Be 1
@@ -129,56 +151,67 @@ Describe "Azure ARM Template Unit Tests" {
         It "Role Assignments Created" {
             $roleAssignments = $resourceMap['Microsoft.Authorization/roleAssignments']
 
-            $roleAssignments.Count | Should -Be 1
-            $roleAssignments[0].ChangeType | Should -Be "Create"
-            $roleAssignments[0].After["name"].Value | Should -Be $SCDMInputId
+            $roleAssignments.Count | Should -Be $regions.Count
 
-            $properties = $roleAssignments[0].After["properties"]
-            $properties["principalId"].Value | Should -Be $ServicePrincipalObjectId
-            $properties["roleDefinitionId"].Value | Should -Be $resourceMap['Microsoft.Authorization/roleDefinitions'][0].After["id"].Value
+            foreach($roleAssignment in $roleAssignments) {
+                $roleAssignment.ChangeType | Should -Be "Create"
+                $properties = $roleAssignment.After["properties"]
+                $properties["principalId"].Value | Should -Be $ServicePrincipalObjectId
+                $properties["roleDefinitionId"].Value | Should -Be $resourceMap['Microsoft.Authorization/roleDefinitions'][0].After["id"].Value
+            }
         }
 
         It "Eventhub Namespace Created" {
             $eventhubNamespaces = $resourceMap['Microsoft.EventHub/namespaces']
+            $eventHubNamespaceNamePrefixPattern = $eventHubNamespaceNamePrefix + '*'
 
-            $eventhubNamespaces.Count | Should -Be 1
-            $eventhubNamespaces[0].ChangeType | Should -Be "Create"
-            $eventhubNamespaces[0].After["name"].Value | Should -Be $eventHubNamespaceName
-            $eventhubNamespaces[0].After["tags"]["SplunkDMInputId"].Value | Should -Be $SCDMInputId
-            $eventhubNamespaces[0].After["tags"]["SplunkDMDeletionOrder"].Value | Should -Be "5"
-            $eventhubNamespaces[0].After["tags"]["tagKey"].Value | Should -Be "tagValue"
+            $eventhubNamespaces.Count | Should -Be $regions.Count
 
-            $properties = $eventhubNamespaces[0].After["properties"]
-            $properties["isAutoInflateEnabled"].Value | Should -BeTrue
-            $properties["maximumThroughputUnits"].Value | Should -Be 40
-
-            $sku = $eventhubNamespaces[0].After["sku"]
-            $sku["name"].Value | Should -Be "Standard"
+            foreach($eventhubNamespace in $eventhubNamespaces) {
+                $eventhubNamespace.ChangeType | Should -Be "Create"
+                $eventhubNamespace.After["name"].Value | Should -BeLike $eventHubNamespaceNamePrefixPattern
+                $eventhubNamespace.After["tags"]["SplunkDMInputId"].Value | Should -Be $SCDMInputId
+                $eventhubNamespace.After["tags"]["tagKey"].Value | Should -Be "tagValue"
+                
+                $properties = $eventhubNamespace.After["properties"]
+                $properties["isAutoInflateEnabled"].Value | Should -BeTrue
+                $properties["maximumThroughputUnits"].Value | Should -Be 40
+                
+                $sku = $eventhubNamespace.After["sku"]
+                $sku["name"].Value | Should -Be "Standard"
+            }
         }
 
         It "Eventhub Created" {
             $eventHubs = $resourceMap['Microsoft.EventHub/namespaces/eventhubs']
 
-            $eventHubs.Count | Should -Be 1
-            $eventHubs[0].ChangeType | Should -Be "Create"
-            $eventHubs[0].After["name"].Value | Should -Be $eventHubName
+            $eventHubs.Count | Should -Be $regions.Count
 
-            $properties = $eventHubs[0].After["properties"]
-            $properties["partitionCount"].Value | Should -Be 32
+            foreach($eventHub in $eventHubs) {
+                $eventHub.ChangeType | Should -Be "Create"
+                $eventHub.After["name"].Value | Should -Be $eventHubName
+                
+                $properties = $eventHub.After["properties"]
+                $properties["partitionCount"].Value | Should -Be 32
+            }
         }
 
         It "Consumer Group Created" {
             $consumerGroups = $resourceMap['Microsoft.EventHub/namespaces/eventhubs/consumergroups']
 
-            $consumerGroups | Should -HaveCount 1
-            $consumerGroups[0].ChangeType | Should -Be "Create"
-            $consumerGroups[0].After["name"].Value | Should -Be $consumerGroupName
+            $consumerGroups | Should -HaveCount $regions.Count
+
+            foreach($consumergroup in $consumerGroups) {
+                $consumerGroup.ChangeType | Should -Be "Create"
+                $consumerGroup.After["name"].Value | Should -Be $consumerGroupName
+            }
+            
         }
 
         It "Authorization Rules Created" {
             $authRules = $resourceMap['Microsoft.EventHub/namespaces/AuthorizationRules']
 
-            $authRules | Should -HaveCount 2
+            $authRules | Should -HaveCount (2 * $regions.Count)
 
             foreach($authRule in $authRules) {
                 $authRule.ChangeType | Should -Be "Create"
@@ -203,7 +236,7 @@ Describe "Azure ARM Template Unit Tests" {
         It "Storage Account Created" {
             $storageAccounts = $resourceMap['Microsoft.Storage/storageAccounts']
 
-            $storageAccounts | Should -HaveCount 3
+            $storageAccounts | Should -HaveCount (3 * $regions.Count)
             foreach ($storageAccount in $storageAccounts) {
                 $storageAccount.ChangeType | Should -Be "Create"
                 $storageAccount.After["sku"]["name"].Value | Should -Be "Standard_LRS"
@@ -215,26 +248,32 @@ Describe "Azure ARM Template Unit Tests" {
         It "App Service Plan Created" {
             $appServicePlans = $resourceMap['Microsoft.Web/serverfarms']
 
-            $appServicePlans | Should -HaveCount 1
-            $appServicePlans[0].ChangeType | Should -Be "Create"
-            $appServicePlans[0].After["kind"].Value | Should -Be "functionapp"
-            $appServicePlans[0].After["tags"]["SplunkDMInputId"].Value | Should -Be $SCDMInputId
-            $appServicePlans[0].After["tags"]["SplunkDMDeletionOrder"].Value | Should -Be "2"
-            $appServicePlans[0].After["tags"]["tagKey"].Value | Should -Be "tagValue"
-            $appServicePlans[0].After["sku"]["name"].Value | Should -Be "Y1"
-            $appServicePlans[0].After["properties"]["reserved"].Value | Should -Be $true
+            $appServicePlans | Should -HaveCount $regions.Count
+
+            foreach ($appServicePlan in $appServicePlans) {
+                $appServicePlan.ChangeType | Should -Be "Create"
+                $appServicePlan.After["kind"].Value | Should -Be "functionapp"
+                $appServicePlan.After["tags"]["SplunkDMInputId"].Value | Should -Be $SCDMInputId
+                $appServicePlan.After["tags"]["tagKey"].Value | Should -Be "tagValue"
+                $appServicePlan.After["sku"]["name"].Value | Should -Be "Y1"
+                $appServicePlan.After["properties"]["reserved"].Value | Should -Be $true
+            }
         }
 
         It "Function Created" {
             $functions = $resourceMap['Microsoft.Web/sites']
 
-            $functions | Should -HaveCount 1
-            $functions[0].ChangeType | Should -Be "Create"
-            $functions[0].After["name"].Value | Should -Be $functionName
-            $functions[0].After["kind"].Value | Should -Be "functionapp,linux"
-            $functions[0].After["tags"]["SplunkDMInputId"].Value | Should -Be $SCDMInputId
-            $functions[0].After["tags"]["SplunkDMDeletionOrder"].Value | Should -Be "1"
-            $functions[0].After["tags"]["tagKey"].Value | Should -Be "tagValue"
+            $functions | Should -HaveCount $regions.Count
+
+            $functionNamePrefixPattern = $functionNamePrefix + '*'
+
+            foreach ($function in $appServicePlans) {
+                $function.ChangeType | Should -Be "Create"
+                $function.After["name"].Value | Should -BeLike $functionNamePrefixPattern
+                $function.After["kind"].Value | Should -Be "functionapp,linux"
+                $function.After["tags"]["SplunkDMInputId"].Value | Should -Be $SCDMInputId
+                $function.After["tags"]["tagKey"].Value | Should -Be "tagValue"
+            }
         }
     }
 }
